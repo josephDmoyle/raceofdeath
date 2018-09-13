70fa2c73c13c931418f4f47b4c45b264587fed8a
josephDmoyle <u0929730@utah.edu>
2018-09-13 12:26:37 -0600

    floating
    graves solid

--- DeathRacer/Input/app.js
+++ DeathRacer/Input/app.js
@@ -1,151 +1,176 @@
 /// <reference path="phaser.d.ts"/>
-// Demonstrate the use of arrow keys in a Phaser app
-// This application demonstrates creation of a Cursor and polling for input
-var SimpleGame = /** @class */ (function () {
-    //Creates the game graphically
-    function SimpleGame() {
-        this.game = new Phaser.Game(640, 480, Phaser.CANVAS, 'content', {
-            create: this.create, preload: this.preload,
-            update: this.update, render: this.render
-        });
+var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'content', {
+    create: create, preload: preload,
+    update: update, render: render
+});
+//Car sprite
+var car1;
+//Secret Second Player
+var car2;
+//The guys to hit
+var guys;
+//The graves to hit
+var graves;
+//The four arrows
+var cursors;
+//W key
+var W;
+//A key
+var A;
+//S key
+var S;
+//D key
+var D;
+//Space key
+var Space;
+//Scream sound
+var s1;
+//Text object for display
+var scoreText;
+//Kill count
+var score;
+//Text object for displaying the time
+var timeText;
+//Timer object for displaying the time
+var timer;
+//Time Count
+var time;
+//Load in all the graphical content
+function preload() {
+    game.load.image("racer", "racer.png");
+    game.load.image("guy0", "guy0.png");
+    game.load.image("guy1", "guy1.png");
+    game.load.image("dead", "dead.png");
+    game.load.spritesheet("guy", "guy.png", 32, 32, 2);
+    game.load.audio("s1", "s1.mp3");
+}
+//Initialize all the objects within the game
+function create() {
+    //Initiate the physics engine
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    scoreText = game.add.text(32, 32, "KILLS: ");
+    scoreText.fill = "white";
+    score = 0;
+    timeText = game.add.text(32, 64, "TIME: ");
+    timeText.fill = "white";
+    timer = game.time.create(false);
+    timer.loop(1000, updateCounter, this);
+    timer.start();
+    time = 30;
+    guys = game.add.group();
+    guys.enableBody = true;
+    graves = game.add.group();
+    graves.enableBody = true;
+    s1 = game.add.audio("s1");
+    s1.allowMultiple = true;
+    for (var i = 0; i < 2; i++) {
+        var s = guys.create(game.world.randomX, game.world.randomY, 'guy', 5);
+        s.name = 'guy' + s;
+        s.body.collideWorldBounds = true;
+        s.body.bounce.setTo(0.8, 0.8);
+        s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+        s.anchor.set(0.5);
+        var anim = s.animations.add("guy");
+        anim.play(10, true);
     }
-    //Load in all the graphical content
-    SimpleGame.prototype.preload = function () {
-        this.game.load.image("racer", "racer.png");
-        this.game.load.image("guy0", "guy0.png");
-        this.game.load.image("guy1", "guy1.png");
-        this.game.load.image("dead", "dead.png");
-        this.game.load.spritesheet("guy", "guy.png", 32, 32, 2);
-        this.game.load.audio("s1", "s1.mp3");
-    };
-    //Initialize all the objects within the game
-    SimpleGame.prototype.create = function () {
-        //Initiate the physics engine
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.scoreText = this.game.add.text(32, 32, "KILLS: ");
-        this.scoreText.fill = "white";
-        this.score = 0;
-        this.timeText = this.game.add.text(32, 64, "TIME: ");
-        this.timeText.fill = "white";
-        this.timer = this.game.time.create(false);
-        this.timer.loop(30000, this.updateCounter, this);
-        this.timer.start();
-        this.guys = this.game.add.group();
-        this.guys.enableBody = true;
-        this.s1 = this.game.add.audio("s1");
-        this.s1.allowMultiple = true;
-        for (var i = 0; i < 2; i++) {
-            var s = this.guys.create(this.game.world.randomX, this.game.world.randomY, 'guy', 5);
-            s.name = 'guy' + s;
-            s.body.collideWorldBounds = true;
-            s.body.bounce.setTo(0.8, 0.8);
-            s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
-            s.anchor.set(0.5);
-            var anim = s.animations.add("guy");
-            anim.play(10, true);
-        }
-        //Load in the image of the racer to get the proper dimensions
-        var carImage = this.game.cache.getImage("racer");
-        //Create the car as a sprite with the loaded content
-        this.car = this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, "racer");
-        //Create the car as a sprite with the loaded content
-        //this.car2 = this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, "racer");
-        //Set the pivot point to the center of the car
-        this.car.anchor.set(0.5);
-        //Set the pivot point to the center of the car
-        //this.car2.anchor.set(0.5);
-        //Enable the arcade physics interactions
-        this.game.physics.arcade.enable(this.car);
-        //Enable the arcade physics interactions
-        //this.game.physics.arcade.enable(this.car2);
-        //Set car to stay on screen
-        this.car.body.collideWorldBounds = true;
-        //Set the bounciness of the car
-        this.car.body.bounce.set(0.8);
-        //Allow the car body to be rotated by us
-        this.car.body.allowRotation = true;
-        //Set the car to be infinite weight
-        this.car.body.immovable = true;
-        //Set car to stay on screen
-        //this.car2.body.collideWorldBounds = true;
-        //Set the bounciness of the car
-        //this.car2.body.bounce.set(0.8);
-        //Allow the car body to be rotated by us
-        //this.car2.body.allowRotation = true;
-        //Set the car to be infinite weight
-        //this.car2.body.immovable = true;
-        // create the cursor key object
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-        // create the WASD movements
-        this.W = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
-        this.A = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
-        this.S = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
-        this.D = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
-        this.Space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    };
-    SimpleGame.prototype.update = function () {
-        // Update input state
-        this.game.input.update();
-        //this.game.physics.arcade.collide(this.car, this.car2);
+    //Load in the image of the racer to get the proper dimensions
+    var carImage = game.cache.getImage("racer");
+    //Create the car as a sprite with the loaded content
+    car1 = game.add.sprite(game.world.randomX, game.world.randomY, "racer");
+    //Create the car as a sprite with the loaded content
+    //car2 = game.add.sprite(game.world.randomX, game.world.randomY, "racer");
+    //Set the pivot point to the center of the car
+    car1.anchor.set(0.5);
+    //Set the pivot point to the center of the car
+    //car2.anchor.set(0.5);
+    //Enable the arcade physics interactions
+    game.physics.arcade.enable(car1);
+    //Enable the arcade physics interactions
+    //game.physics.arcade.enable(car2);
+    //Set car to stay on screen
+    car1.body.collideWorldBounds = true;
+    //Set the bounciness of the car
+    car1.body.bounce.set(0.8);
+    //Allow the car body to be rotated by us
+    car1.body.allowRotation = true;
+    //Set car to stay on screen
+    //car2.body.collideWorldBounds = true;
+    //Set the bounciness of the car
+    //car2.body.bounce.set(0.8);
+    //Allow the car body to be rotated by us
+    //car2.body.allowRotation = true;
+    // create the cursor key object
+    cursors = game.input.keyboard.createCursorKeys();
+    // create the WASD movements
+    W = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    A = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    S = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    D = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    Space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+}
+function update() {
+    // Update input state
+    game.input.update();
+    if (time > 0) {
         //Process collisions with car to guys
-        this.guys.forEachAlive(function (guy) {
-            //if (this.game.physics.arcade.collide(this.car, guy) || this.game.physics.arcade.collide(this.car2, guy)) {
-            if (this.game.physics.arcade.collide(this.car, guy)) {
-                this.score++;
-                var grave = this.game.add.sprite(guy.x, guy.y, "dead");
-                grave.scale.set(guy.scale.x, 1);
-                guy.x = this.game.world.randomX;
-                guy.y = this.game.world.randomY;
-                guy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
-                this.s1.play();
-            }
-            if (guy.body.velocity.x < 0) {
-                guy.scale.set(-1, 1);
-            }
-            else {
-                guy.scale.set(1, 1);
-            }
-        }, this);
+        game.physics.arcade.collide(car1, graves);
+        //Process collisions with car to guys
+        game.physics.arcade.collide(car1, guys, vehicularManslaughter, null);
         //Set velocities to zero so we can directly manipulate them each frame
-        this.car.body.velocity.x = 0;
-        this.car.body.velocity.y = 0;
-        this.car.body.angularVelocity = 0;
+        car1.body.velocity.x = 0;
+        car1.body.velocity.y = 0;
+        car1.body.angularVelocity = 0;
         //Set velocities to zero so we can directly manipulate them each frame
-        //this.car2.body.velocity.x = 0;
-        //this.car2.body.velocity.y = 0;
-        //this.car2.body.angularVelocity = 0;
+        //car2.body.velocity.x = 0;
+        //car2.body.velocity.y = 0;
+        //car2.body.angularVelocity = 0;
         //Angular rotations given by A/l and D/r
-        if (this.A.isDown)
-            this.car.body.angularVelocity = -200;
-        else if (this.D.isDown)
-            this.car.body.angularVelocity = 200;
+        if (A.isDown)
+            car1.body.angularVelocity = -200;
+        else if (D.isDown)
+            car1.body.angularVelocity = 200;
         //Driving pedals given by W/u and S/d
-        if (this.W.isDown)
-            this.car.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.car.angle, 300));
-        else if (this.S.isDown)
-            this.car.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.car.angle, -100));
+        if (W.isDown)
+            car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, 300));
+        else if (S.isDown)
+            car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, -100));
         //Angular rotations given by A/l and D/r
-        //if (this.cursors.left.isDown)
-        //  this.car2.body.angularVelocity = -200;
-        //else if (this.cursors.right.isDown)
-        //  this.car2.body.angularVelocity = 200;
+        //if (cursors.left.isDown)
+        //  car2.body.angularVelocity = -200;
+        //else if (cursors.right.isDown)
+        //  car2.body.angularVelocity = 200;
         //Driving pedals given by W/u and S/d
-        //if (this.cursors.up.isDown)
-        //  this.car2.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.car2.angle, 300));
-        //else if (this.cursors.down.isDown)
-        //  this.car2.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.car2.angle, -100));
-    };
-    SimpleGame.prototype.render = function () {
-        this.scoreText.text = "KILLS: " + this.score;
-        var tiempo = Math.abs(30 - this.timer.seconds).toFixed(0);
-        this.timeText.text = "TIME: " + tiempo;
-    };
-    SimpleGame.prototype.updateCounter = function () {
-        var q = 0;
-    };
-    return SimpleGame;
-}());
-window.onload = function () {
-    var game = new SimpleGame();
-};
+        //if (cursors.up.isDown)
+        //  car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, 300));
+        //else if (cursors.down.isDown)
+        //  car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, -100));
+    }
+}
+function render() {
+    scoreText.text = "KILLS: " + score;
+    timeText.text = "TIME: " + time;
+    guys.forEachAlive(function (guy) {
+        if (guy.body.velocity.x < 0) {
+            guy.scale.set(-1, 1);
+        }
+        else {
+            guy.scale.set(1, 1);
+        }
+    }, this);
+}
+function vehicularManslaughter(car, guy) {
+    score++;
+    var grave = graves.create(guy.x, guy.y, 'dead', 5);
+    grave.anchor.set(0.5);
+    grave.scale.set(guy.scale.x, 1);
+    grave.body.immovable = true;
+    guy.x = game.world.randomX;
+    guy.y = game.world.randomY;
+    guy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+    s1.play();
+}
+function updateCounter() {
+    if (time > 0)
+        time--;
+}
 //# sourceMappingURL=app.js.map
