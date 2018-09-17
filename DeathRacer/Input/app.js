602fa4a26f3c4608335f170c6cf5775498905677
josephDmoyle <u0929730@utah.edu>
2018-09-17 17:46:23 -0600

    waves added

--- DeathRacer/Input/app.js
+++ DeathRacer/Input/app.js
@@ -29,12 +29,18 @@ var s1;
 var scoreText;
 //Kill count
 var score;
+//Prev Score
+var prevScore;
 //Text object for displaying the time
 var timeText;
 //Timer object for displaying the time
 var timer;
 //Time Count
 var time;
+//0 for base, 1 for clearing
+var state;
+//Global round
+var round;
 //Load in all the graphical content
 function preload() {
     game.load.image("racer", "racer.png");
@@ -51,19 +57,22 @@ function create() {
     scoreText = game.add.text(32, 32, "KILLS: ");
     scoreText.fill = "white";
     score = 0;
+    prevScore = score;
+    round = 0;
+    state = 0;
     timeText = game.add.text(32, 64, "TIME: ");
     timeText.fill = "white";
     timer = game.time.create(false);
     timer.loop(1000, updateCounter, this);
     timer.start();
-    time = 30;
+    time = 10;
     guys = game.add.group();
     guys.enableBody = true;
     graves = game.add.group();
     graves.enableBody = true;
     s1 = game.add.audio("s1");
     s1.allowMultiple = true;
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < 6; i++) {
         var s = guys.create(game.world.randomX, game.world.randomY, 'guy', 5);
         s.name = 'guy' + s;
         s.body.collideWorldBounds = true;
@@ -78,15 +87,15 @@ function create() {
     //Create the car as a sprite with the loaded content
     car1 = game.add.sprite(game.world.randomX, game.world.randomY, "racer");
     //Create the car as a sprite with the loaded content
-    //car2 = game.add.sprite(game.world.randomX, game.world.randomY, "racer");
+    car2 = game.add.sprite(game.world.randomX, game.world.randomY, "racer");
     //Set the pivot point to the center of the car
     car1.anchor.set(0.5);
     //Set the pivot point to the center of the car
-    //car2.anchor.set(0.5);
+    car2.anchor.set(0.5);
     //Enable the arcade physics interactions
     game.physics.arcade.enable(car1);
     //Enable the arcade physics interactions
-    //game.physics.arcade.enable(car2);
+    game.physics.arcade.enable(car2);
     //Set car to stay on screen
     car1.body.collideWorldBounds = true;
     //Set the bounciness of the car
@@ -94,11 +103,11 @@ function create() {
     //Allow the car body to be rotated by us
     car1.body.allowRotation = true;
     //Set car to stay on screen
-    //car2.body.collideWorldBounds = true;
+    car2.body.collideWorldBounds = true;
     //Set the bounciness of the car
-    //car2.body.bounce.set(0.8);
+    car2.body.bounce.set(0.8);
     //Allow the car body to be rotated by us
-    //car2.body.allowRotation = true;
+    car2.body.allowRotation = true;
     // create the cursor key object
     cursors = game.input.keyboard.createCursorKeys();
     // create the WASD movements
@@ -111,39 +120,46 @@ function create() {
 function update() {
     // Update input state
     game.input.update();
-    if (time > 0) {
-        //Process collisions with car to guys
-        game.physics.arcade.collide(car1, graves);
-        //Process collisions with car to guys
-        game.physics.arcade.collide(car1, guys, vehicularManslaughter, null);
-        //Set velocities to zero so we can directly manipulate them each frame
-        car1.body.velocity.x = 0;
-        car1.body.velocity.y = 0;
-        car1.body.angularVelocity = 0;
-        //Set velocities to zero so we can directly manipulate them each frame
-        //car2.body.velocity.x = 0;
-        //car2.body.velocity.y = 0;
-        //car2.body.angularVelocity = 0;
-        //Angular rotations given by A/l and D/r
-        if (A.isDown)
-            car1.body.angularVelocity = -200;
-        else if (D.isDown)
-            car1.body.angularVelocity = 200;
-        //Driving pedals given by W/u and S/d
-        if (W.isDown)
-            car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, 300));
-        else if (S.isDown)
-            car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, -100));
-        //Angular rotations given by A/l and D/r
-        //if (cursors.left.isDown)
-        //  car2.body.angularVelocity = -200;
-        //else if (cursors.right.isDown)
-        //  car2.body.angularVelocity = 200;
-        //Driving pedals given by W/u and S/d
-        //if (cursors.up.isDown)
-        //  car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, 300));
-        //else if (cursors.down.isDown)
-        //  car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, -100));
+    game.physics.arcade.collide(guys, graves);
+    switch (state) {
+        case 0:
+            //Process collisions with car to guys
+            game.physics.arcade.collide(car1, graves);
+            //Process collisions with car to guys
+            game.physics.arcade.collide(car1, guys, vehicularManslaughter, null);
+            //Set velocities to zero so we can directly manipulate them each frame
+            car1.body.velocity.x = 0;
+            car1.body.velocity.y = 0;
+            car1.body.angularVelocity = 0;
+            //Angular rotations given by A/l and D/r
+            if (A.isDown)
+                car1.body.angularVelocity = -200;
+            else if (D.isDown)
+                car1.body.angularVelocity = 200;
+            //Driving pedals given by W/u and S/d
+            if (W.isDown)
+                car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, 300));
+            else if (S.isDown)
+                car1.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car1.angle, -100));
+            break;
+        default:
+            //Process collisions with car to guys
+            game.physics.arcade.collide(car2, graves, graveRobber, null);
+            //Set velocities to zero so we can directly manipulate them each frame
+            car2.body.velocity.x = 0;
+            car2.body.velocity.y = 0;
+            car2.body.angularVelocity = 0;
+            //Angular rotations given by A/l and D/r
+            if (A.isDown)
+                car2.body.angularVelocity = -200;
+            else if (D.isDown)
+                car2.body.angularVelocity = 200;
+            //Driving pedals given by W/u and S/d
+            if (W.isDown)
+                car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, 300));
+            else if (S.isDown)
+                car2.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(car2.angle, -100));
+            break;
     }
 }
 function render() {
@@ -169,8 +185,44 @@ function vehicularManslaughter(car, guy) {
     guy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
     s1.play();
 }
+function graveRobber(car, grave) {
+    grave.kill();
+}
 function updateCounter() {
-    if (time > 0)
-        time--;
+    switch (state) {
+        case 0:
+            if (time > 0)
+                time--;
+            if (time == 0) {
+                state = 1;
+                time = 5;
+                round++;
+            }
+            if (score == prevScore) {
+                //you lose
+            }
+            break;
+        default:
+            if (time > 0)
+                time--;
+            if (time == 0) {
+                prevScore = score;
+                state = 0;
+                time = 10;
+                if (round % 6 == 0) {
+                    for (var i = 0; i < 6; i++) {
+                        var s = guys.create(game.world.randomX, game.world.randomY, 'guy', 5);
+                        s.name = 'guy' + s;
+                        s.body.collideWorldBounds = true;
+                        s.body.bounce.setTo(0.8, 0.8);
+                        s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+                        s.anchor.set(0.5);
+                        var anim = s.animations.add("guy");
+                        anim.play(10, true);
+                    }
+                }
+            }
+            break;
+    }
 }
 //# sourceMappingURL=app.js.map
